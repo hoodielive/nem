ae9e7b507c95233b7bc7d4c40a5da2208f9af0d9
hoodielive <hoodielive@gmail.com>
2018-05-31 21:32:05 -0400

    trying to fix this error with put request for edits

--- vidjot/app.js
+++ vidjot/app.js
@@ -2,114 +2,144 @@
 const express = require('express');
 
 // Express Handlebars
-const exphbs = require('express-handlebars'); 
+const exphbs = require('express-handlebars');
+
+// Method Override 
+const methodOverride = require('method-override');
 
 // Mongoose 
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 
 // Body-Parser
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 
 // App 
 const app = express();
 
 // Map global promise - get rid of warning
-mongoose.Promise = global.Promise; 
+mongoose.Promise = global.Promise;
 
 // Connect to mongoose 
 mongoose.connect('mongodb://localhost/vidjot-dev')
 
-  .then(() => console.log('MongoDB connected...')) 
-  .catch(err => console.log(err)); 
+	.then(() => console.log('MongoDB connected...'))
+	.catch(err => console.log(err));
 
 // Load IdeaSchema model
-require('./models/Idea'); 
-const Idea = mongoose.model('ideas'); 
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 // Handlebars Middleware 
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 }));
 
-app.set('view engine', 'handlebars'); 
+app.set('view engine', 'handlebars');
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false })) 
-app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(bodyParser.json());
+
+// Method Override middleware
+app.use(methodOverride('_method'));
 
 // Index Route 
 const title = 'Welcome'
 app.get('/', (req, res) => {
-	res.render('index',  { 
+	res.render('index', {
 		title: title
-	}); 
-}); 
+	});
+});
 
 // About Route
 app.get('/about', (req, res) => {
-    res.render('about'); 
+	res.render('about');
 });
 
 // Idea Index Page 
 app.get('/ideas', (req, res) => {
 	Idea.find({})
-		.sort({date: 'desc'})
+		.sort({
+			date: 'desc'
+		})
 		.then(ideas => {
 			res.render('ideas/index', {
-				ideas:ideas
-			}); 
+				ideas: ideas
+			});
 		});
-}); 
+});
 
 // Add Idea form
 app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add'); 
+	res.render('ideas/add');
 });
 
 // Edit Idea Form 
 app.get('/ideas/edit/:id', (req, res) => {
 	Idea.findOne({
-		_id: req.params.id
-	})
-	.then(idea => {
-		res.render('ideas/edit', {
-			idea:idea
+			_id: req.params.id
+		})
+		.then(idea => {
+			res.render('ideas/edit', {
+				idea: idea
+			});
 		});
-	});
 });
 
 // Add Process form
 app.post('/ideas', (req, res) => {
-	let errors = []; 
+	let errors = [];
 
 	if (!req.body.title) {
-		errors.push({text: 'Please add a title'}); 
-  }
-	if (!req.body.details) { 
-		errors.push({text: 'Please add some data'}); 
+		errors.push({
+			text: 'Please add a title'
+		});
+	}
+	if (!req.body.details) {
+		errors.push({
+			text: 'Please add some data'
+		});
 	}
 
 	if (errors.length > 0) {
 		res.render('ideas/add', {
-			errors: errors, 
+			errors: errors,
 			title: req.body.title,
 			details: req.body.details,
-		}); 
+		});
 	} else {
 		const newUser = {
 			title: req.body.title,
-			details: req.body.details, 
+			details: req.body.details,
 		}
 		new Idea(newUser)
-		.save()
+			.save()
+			.then(idea => {
+				res.redirect('/ideas');
+			})
+	}
+});
+
+// Edit Process Form 
+app.put('ideas/:id', (req, res) => {
+	Idea.findOne({
+		_id: req.params.id
+	})
+	.then(idea => {
+		idea.title = req.body.title; 
+		idea.details = req.body.details; 
+
+		idea.save()
 		.then(idea => {
-			res.redirect('/ideas'); 
-		}) 
-	} 
-}); 
+			res.redirect('/ideas')
+		});
+	});
+});
 
 const port = 5000;
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+	console.log(`Server started on port ${port}`);
 
 });
